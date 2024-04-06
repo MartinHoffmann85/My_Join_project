b77743b3d267bb8f8be2445cbcf92daf20327bb0
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-06 06:10:35 +0200

    Header drop down menu  eventlistener added

--- js/header.js
+++ js/header.js
@@ -5,4 +5,16 @@ function toggleDropdown() {
     } else {
       dropdown.style.display = "none";
     }
-  }
+}
+
+
+document.addEventListener("click", function(event) {
+let headerProfilContainer = document.getElementById("headerProfilID");
+let dropdown = document.getElementById("dropdown");
+let targetElement = event.target;
+if (!headerProfilContainer.contains(targetElement)) {    
+    if (dropdown.style.display === "block") {    
+    dropdown.style.display = "none";
+    }
+}
+});
