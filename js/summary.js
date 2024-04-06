cfe43d8eb3bfaf312364a1867563cb9dad189b54
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-06 17:27:09 +0200

    summary.js counts for the tasks added works fine

--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,109 @@
-setTimeout(showHeaderUserInitials, 200);
+function summaryInit() {
+    setTimeout(showHeaderUserInitials, 200);
+    updateGreeting();
+    getSummaryToDosCount();
+    getDoneTasksCount();
+    summaryGetUrgentPriorityCount();
+    summaryGetAllTasksCount();
+    getInProgressTasksCount();
+    summaryGetAwaitingFeedbackCount();
+}
+
+
+function getSummaryToDosCount() {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.tasks && Array.isArray(currentUser.tasks)) {
+        const todoTasksCount = currentUser.tasks.filter(task => task.columnId === 'todo').length;
+        const summaryToDoID = document.getElementById('summaryToDoID');
+        if (summaryToDoID) {
+            summaryToDoID.textContent = todoTasksCount.toString();
+        } else {
+            console.error('Summary to-do ID element not found');
+        }
+    } else {
+        console.error('Invalid tasks data in localStorage');
+    }
+}
+
+
+function getDoneTasksCount() {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  if (currentUser && currentUser.tasks && Array.isArray(currentUser.tasks)) {
+      const doneTasksCount = currentUser.tasks.filter(task => task.columnId === 'done').length;
+      const summaryDoneID = document.getElementById('summaryDoneID');
+      if (summaryDoneID) {
+          summaryDoneID.textContent = doneTasksCount.toString();
+      } else {
+          console.error('Summary done ID element not found');
+      }
+  } else {
+      console.error('Invalid tasks data in localStorage');
+  }
+}
+
+
+function summaryGetUrgentPriorityCount() {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  if (currentUser && currentUser.tasks && Array.isArray(currentUser.tasks)) {
+      const urgentPriorityCount = currentUser.tasks.filter(task => task.prio === 'urgent').length;
+      const summaryPriorityID = document.getElementById('summaryPriorityID');
+      if (summaryPriorityID) {
+          summaryPriorityID.textContent = urgentPriorityCount.toString();
+      } else {
+          console.error('Summary Priority ID element not found');
+      }
+  } else {
+      console.error('Invalid tasks data in localStorage');
+  }
+}
+
+
+function summaryGetAllTasksCount() {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  if (currentUser && currentUser.tasks && Array.isArray(currentUser.tasks)) {
+      const taskCount = currentUser.tasks.length;
+      const summaryToDoID = document.getElementById('summaryAllTasksID');
+      if (summaryToDoID) {
+          summaryToDoID.textContent = taskCount.toString();
+      } else {
+          console.error('Summary to-do ID element not found');
+      }
+  } else {
+      console.error('Invalid tasks data in localStorage');
+  }
+}
+
+
+function getInProgressTasksCount() {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  if (currentUser && currentUser.tasks && Array.isArray(currentUser.tasks)) {
+      const inProgressTasksCount = currentUser.tasks.filter(task => task.columnId === 'inprogress').length;
+      const summaryInProgressID = document.getElementById('summaryProgressID');
+      if (summaryInProgressID) {
+          summaryInProgressID.textContent = inProgressTasksCount.toString();
+      } else {
+          console.error('Summary in Progress ID element not found');
+      }
+  } else {
+      console.error('Invalid tasks data in localStorage');
+  }
+}
+
+
+function summaryGetAwaitingFeedbackCount() {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  if (currentUser && currentUser.tasks && Array.isArray(currentUser.tasks)) {
+      const awaitingFeedbackCount = currentUser.tasks.filter(task => task.columnId === 'awaitfeedback').length;
+      const summaryFeedbackID = document.getElementById('summaryFeedbackID');
+      if (summaryFeedbackID) {
+          summaryFeedbackID.textContent = awaitingFeedbackCount.toString();
+      } else {
+          console.error('Summary Feedback ID element not found');
+      }
+  } else {
+      console.error('Invalid tasks data in localStorage');
+  }
+}
 
 
 async function updateGreeting() {
@@ -27,21 +132,5 @@ async function updateGreeting() {
     }  
     const waitTime = addHours * 60 * 60 * 1000 + addMinutes * 60 * 1000;    
     setTimeout(updateGreeting, waitTime)
-  }
-  
-  updateGreeting();
-  
-  function button() {
-    const currentUser = requestToBackend() ;    
 }
-
-
-async function requestToBackend() {
-    try {
-        const result = await getItem('currentUser');
-        return JSON.parse(result) || [];
-    } catch (e) {
-        console.error('Loading error:', e);
-        return [];
-    }
-}
+  
