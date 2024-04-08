61307dcc746e0dd12525a7e6abd1388568327527
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-08 17:53:34 +0200

    header.js unused functions removed and clean code final

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 function toggleDropdown() {
-    var dropdown = document.getElementById("dropdown");
+    let dropdown = document.getElementById("dropdown");
     if (dropdown.style.display === "none" || dropdown.style.display === "") {
       dropdown.style.display = "block";
     } else {
