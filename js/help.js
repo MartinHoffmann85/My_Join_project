2e2c21450daef5d610e4d4edcf6ab46c0ccf31ca
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-09 16:42:34 +0200

    help.js coments with JSDoc

--- js/help.js
+++ js/help.js
@@ -1,8 +1,14 @@
+/**
+ * Initializes the help section by showing the header user initials after a delay.
+ */
 function helpInit() {
     setTimeout(showHeaderUserInitials, 500);
 }
 
 
+/**
+ * Redirects the user to the summary page.
+ */
 function backToSummary() {
     window.location.assign("./summary.html");
 }
