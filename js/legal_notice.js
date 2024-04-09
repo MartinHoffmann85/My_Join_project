efbbedb795dd222686a11076091b8f839325a679
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-09 16:45:33 +0200

    legal_notice.js coments with JSDoc

--- js/legal_notice.js
+++ js/legal_notice.js
@@ -1,8 +1,14 @@
+/**
+ * Initializes the legal notice page by showing header user initials after a delay.
+ */
 function legalNoticeInit() {
     setTimeout(showHeaderUserInitials, 500);
 }
 
 
+/**
+ * Redirects the user back to the summary page.
+ */
 function backToSummary() {
     window.location.assign("./summary.html");
 }
