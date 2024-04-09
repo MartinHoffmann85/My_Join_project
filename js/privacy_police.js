3d9b6512e44630c585f2e160895704da826e1b0c
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-09 16:46:33 +0200

    privacy_police.js coments with JSDoc

--- js/privacy_police.js
+++ js/privacy_police.js
@@ -1,8 +1,14 @@
+/**
+ * Initializes the privacy policy page by showing header user initials after a delay.
+ */
 function privacyPolicyInit() {
     setTimeout(showHeaderUserInitials, 500);
 }
 
 
+/**
+ * Redirects the user back to the summary page.
+ */
 function backToSummary() {
     window.location.assign("./summary.html");
 }
