33bec258e9d62456db3037eeea2b5a775b0a1596
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-09 16:44:25 +0200

    includeHTML.js coments with JSDoc

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,10 +1,14 @@
+/**
+ * Includes HTML content in elements with the attribute 'w3-include-html'.
+ * Fetches the HTML file specified in the attribute and inserts its content into the element.
+ */
 async function includeHTML() {
     let include = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < include.length; i++) {
         const element = include[i];
         let file = element.getAttribute("w3-include-html");
         let resp = await fetch(file);
-        if(resp.ok) {
+        if (resp.ok) {
             include[i].innerHTML = await resp.text();
         } else {
             include[i].innerHTML = 'Page not found';
