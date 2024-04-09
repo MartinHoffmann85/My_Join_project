326f67b6cedda255f6c36fe9e877798c44dceddc
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-09 16:41:28 +0200

    header.js coments with JSDoc

--- js/header.js
+++ js/header.js
@@ -1,20 +1,27 @@
+/**
+ * Toggles the visibility of a dropdown menu.
+ */
 function toggleDropdown() {
-    let dropdown = document.getElementById("dropdown");
-    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+  let dropdown = document.getElementById("dropdown");
+  if (dropdown.style.display === "none" || dropdown.style.display === "") {
       dropdown.style.display = "block";
-    } else {
+  } else {
       dropdown.style.display = "none";
-    }
+  }
 }
 
 
+/**
+* Closes the dropdown menu when clicking outside of it.
+* @param {Event} event - The click event.
+*/
 document.addEventListener("click", function(event) {
-let headerProfilContainer = document.getElementById("headerProfilID");
-let dropdown = document.getElementById("dropdown");
-let targetElement = event.target;
-if (!headerProfilContainer.contains(targetElement)) {    
-    if (dropdown.style.display === "block") {    
-    dropdown.style.display = "none";
-    }
-}
+  let headerProfilContainer = document.getElementById("headerProfilID");
+  let dropdown = document.getElementById("dropdown");
+  let targetElement = event.target;
+  if (!headerProfilContainer.contains(targetElement)) {    
+      if (dropdown.style.display === "block") {    
+          dropdown.style.display = "none";
+      }
+  }
 });
