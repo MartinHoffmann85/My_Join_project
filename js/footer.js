c92cb632797d63e9d3b4d0e73336439572663806
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-04-21 03:18:28 +0200

    Footer link active effect tryout 2

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-async function footerInit() {    
+function footerInit() {    
     console.log("footerInit called");
     document.addEventListener("DOMContentLoaded", function() {
         // Funktion zum Setzen der Hintergrundfarbe des aktiven Links
@@ -9,14 +9,21 @@ async function footerInit() {
             const link3 = document.getElementById('footer-icon-boxId3');
             const link4 = document.getElementById('footer-icon-boxId4');
 
+            // Entfernen der ":hover" Klasse von allen Link-Boxen
+            link1.classList.remove('footer-icon-box-hover');
+            link2.classList.remove('footer-icon-box-hover');
+            link3.classList.remove('footer-icon-box-hover');
+            link4.classList.remove('footer-icon-box-hover');
+
+            // Hinzufügen der ":hover" Klasse zum aktiven Link
             if (activeLinkId === 'footer-icon-boxId1') {
-                link1.style.backgroundColor = 'var(--focus-color-btn)';
+                link1.classList.add('footer-icon-box-hover');
             } else if (activeLinkId === 'footer-icon-boxId2') {
-                link2.style.backgroundColor = 'var(--focus-color-btn)';
+                link2.classList.add('footer-icon-box-hover');
             } else if (activeLinkId === 'footer-icon-boxId3') {
-                link3.style.backgroundColor = 'var(--focus-color-btn)';
+                link3.classList.add('footer-icon-box-hover');
             } else if (activeLinkId === 'footer-icon-boxId4') {
-                link4.style.backgroundColor = 'var(--focus-color-btn)';
+                link4.classList.add('footer-icon-box-hover');
             }
         };
 
